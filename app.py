7c5ef174f40cf7ccb56e60471fbb1f6cd00c0437
DNagasri <nagasree.dokku@itversity.in>
2020-06-26 16:54:11 +0530

    Added logic to read table list that need to be loaded from a file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 from config import DB_DETAILS
-
+from util import get_tables
 
 def main():
     # for arg in sys.argv:
@@ -8,7 +8,10 @@ def main():
     """Program takes atleast one argument"""
     env = sys.argv[1]
     db_details = DB_DETAILS[env]
-    print(db_details)
+    # print(db_details)
+    tables=get_tables("tables_list")
+    for table in tables['table_name']:
+        print(table)
 
 
 if __name__ =='__main__':
